08cbe77ceee314f59524dcddd13aed7a8292eb4e
Mohammed Anwar <m_sahaly@hotmail.com>
2026-01-19 05:42:00 +0000

    Pipeline Created: with UV version of python

---
+++ pipeline/pipeline.py
@@ -0,0 +1,23 @@
+import sys
+try:
+    import pandas as pd
+    print(f"Pandas is installed! Version: {pd.__version__}")
+except ImportError:
+    print("Pandas is NOT installed.")
+
+    
+
+print("Argument:", sys.argv)
+month = sys.argv[1]
+
+df = pd.DataFrame({"Day":[1, 2], "Num_Passengers":[3,4]})
+
+# Add month Column
+df['month'] = month
+print(df.head())
+
+#convert DF for Parquet file
+df.to_parquet(f"output_{month}.parquet")
+
+
+print(f"Hello pipeline, month: {month}")
